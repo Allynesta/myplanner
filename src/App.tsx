eb44287bbc650c92ee27321a1d6b2d34c636704e
Allynesta <ally_nesta1107@outlook.com>
2024-07-02 23:14:09 +0400

    new

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,8 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import PlannerPage from "./pages/PlannerPage";
 import ReportPage from "./pages/ReportPage";
 import "./styles/nav.css";
-import sampleReportData from "./data/sampleReportData";
 
-// Define the structure of the data each report will have
-interface ReportData {
-	id: number;
-	location: string;
-	description: string;
-	date: Date;
-	pack: number;
-	price: number;
-}
-
-// Define the props that App will receive
-interface Props {
-	reportData?: ReportData[];
-}
-
-const App: React.FC<Props> = () => {
+const App: React.FC = () => {
 	// Implement the delete handler function
 	const handleDelete = (id: number) => {
 		// Handle delete functionality (this can be implemented as needed)
@@ -41,15 +25,7 @@ const App: React.FC<Props> = () => {
 			</nav>
 			<Routes>
 				<Route path="/" element={<PlannerPage />} />
-				<Route
-					path="/Report-Table"
-					element={
-						<ReportPage
-							reportData={sampleReportData} // Pass the raw reportData
-							onDelete={handleDelete}
-						/>
-					}
-				/>
+				<Route path="/Report-Table" element={<ReportPage />} />
 			</Routes>
 		</Router>
 	);
