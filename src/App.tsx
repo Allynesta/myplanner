7f79fb3c51369b0272e770e25fd8b64ffc51effd
Allynesta <ally_nesta1107@outlook.com>
2024-07-02 14:49:36 +0400

    add nav bar to navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import PlannerPage from "./pages/PlannerPage";
 import ReportPage from "./pages/ReportPage";
 
 function App() {
 	return (
 		<Router>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/Report-Table">Report Table</Link>
+					</li>
+				</ul>
+			</nav>
 			<Routes>
 				<Route path="/" element={<PlannerPage />} />
 				<Route path="/Report-Table" element={<ReportPage />} />
