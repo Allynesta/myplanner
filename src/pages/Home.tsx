74220ece7b6e04f5b3a86c8ed4bc1fe25e86acc5
Allynesta <ally_nesta1107@outlook.com>
2024-07-17 11:54:04 +0400

    "Updated CSS files: added global styles, refactored button and form element styles, and made changes to dashboard and report table layouts."

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ const Home = () => {
 		<div>
 			<div className="grid h-screen place-content-center bg-white px-4">
 				<h1 className="uppercase tracking-widest text-gray-500">
-					404 | Not Found
+					404 | My Planner
 				</h1>
 			</div>
 		</div>
