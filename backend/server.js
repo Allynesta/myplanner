ee60beb5a5e98c4459b3ba70ee2ad162ecd75440
Allynesta <ally_nesta1107@outlook.com>
2024-08-13 00:33:35 +0400

    server side

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000' }));
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
