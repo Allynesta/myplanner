116dcdaa14960be01c65c3d2cea51dccf1680437
Allynesta <ally_nesta1107@outlook.com>
2024-08-14 20:27:41 +0400

    test3

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,13 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 const allowedOrigins = [
-    process.env.FRONTEND_URL || 'http://localhost:3000',
-    'https://myplanner-green.vercel.app'
+    'http://localhost:3000', // Local development origin
+    'https://myplanner-green.vercel.app' // Vercel production origin
 ];
 
 app.use(cors({
     origin: function (origin, callback) {
-        // allow requests with no origin, like mobile apps or curl requests
+        // Allow requests with no origin (like mobile apps or curl requests)
         if (!origin) return callback(null, true);
         if (allowedOrigins.indexOf(origin) === -1) {
             const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
@@ -23,6 +23,7 @@ app.use(cors({
         return callback(null, true);
     }
 }));
+
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
